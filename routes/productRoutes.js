26941ab285619b0919d51cb36c66349e899784f0
Zehra Demir <zehrademir.gm@gmail.com>
2024-12-15 18:35:20 +0300

    Create productRoutes.js

---
+++ routes/productRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const { getAllProducts } = require('../controllers/productController');
+const router = express.Router();
+
+router.get('/', getAllProducts);
+
+module.exports = router;
